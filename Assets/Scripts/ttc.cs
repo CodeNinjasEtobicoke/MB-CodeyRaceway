82f938c7443f9b968757cb0f27871f30292251c8
ZestyTaco2011 <astrobrandon2013@gmail.com>
2026-03-26 17:31:32 -0400

    yea smae

--- Assets/Scripts/ttc.cs
+++ Assets/Scripts/ttc.cs
@@ -9,30 +9,45 @@ public class ttc : MonoBehaviour
     public float totalCountdownTime;
     public float totalLapTime;
     public CodeyMove codeyMove;
+
+    private bool raceStarted = false;
+
     void Start()
     {
-        
+        lapTime.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (totalCountdownTime > 0)
+        if (totalCountdownTime > 0 && raceStarted == false)
         {
             totalCountdownTime -= Time.deltaTime;
             startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
             codeyMove.Speed = 0;
         }
-        if (totalCountdownTime <= 0)
+        else if (totalCountdownTime <= 0 && raceStarted == false)
         {
-            startCountdown.text = "";
+            //Stop the Countdown
+            startCountdown.text = " ";
+            totalLapTime -= Time.deltaTime;
+            
+            //Start the Lap Time Countdown
+            codeyMove.Speed = 800;
+            lapTime.text = Mathf.Round(totalLapTime).ToString();
+            raceStarted = true;
+        }
+
+        if (raceStarted == true)
+        {
             totalLapTime -= Time.deltaTime;
             lapTime.text = Mathf.Round(totalLapTime).ToString();
-            codeyMove.Speed = 600;
+
         }
-        if (totalCountdownTime < 0)
+        if (totalLapTime <= 0)
         {
             print("Times up bucko");
+            codeyMove.Speed = 0;
         }
     }
 }
